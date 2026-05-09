8ace68ad2b6a1486aaad8971265b8ca8e3ce1bb4
HamzaDev <hmzt25235@gmail.com>
2026-05-09 12:24:10 +0300

    weird

--- Assets/Player/GameInput.cs
+++ Assets/Player/GameInput.cs
@@ -3,35 +3,21 @@ using UnityEngine;
 public class GameInput : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Vector3 GetMovementVectorNormalize()
+    public Vector3 GetMovementVector()
     {
         Vector3 inputVect = new Vector3(0, 0, 0);
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            Debug.Log("W");
-            inputVect.z = 1;
-        }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            Debug.Log("S");
-            inputVect.z = -1;
-        }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            Debug.Log("A");
-            inputVect.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            Debug.Log("A");
-            inputVect.x = 1;
-        }
-        if (Input.GetKey(KeyCode.Space) && transform.position.y == 0)
-        {
-            inputVect.y = 1;
-        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) inputVect.z = 1;
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) inputVect.z = -1;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) inputVect.x = -1;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) inputVect.x = 1;
 
         return inputVect;
+    }
+
+    public bool IsJumpPressed()
+    {
+        // GetKeyDown returns true only on the frame the button is first pressed
+        return Input.GetKeyDown(KeyCode.Space);
     }
 }
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,7 +5,12 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 9f;
     [SerializeField] private float rotationSpeed = 8f;
+    [SerializeField] private float jumpForce = 120f;
+    [SerializeField] private float gravity = -20.81f;
     [SerializeField] private GameInput gameInput;
+
+    private float verticalVelocity;
+    private bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,11 +21,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 inputVect = gameInput.GetMovementVectorNormalize();
+        Vector3 inputVect = gameInput.GetMovementVector();
 
         inputVect = inputVect.normalized;
-        transform.position += inputVect * Time.deltaTime * moveSpeed;
+        Vector3 moveDir = inputVect * moveSpeed;
 
-        //transform.forward = Vector3.Slerp(transform.forward, inputVect, Time.deltaTime * rotationSpeed);
+        isGrounded = transform.position.y <= 0.1f;
+
+        if (isGrounded && verticalVelocity < 0)
+        {
+            // Reset velocity when hitting the floor
+            verticalVelocity = -2f;
+        }
+
+        if (isGrounded && gameInput.IsJumpPressed())
+        {
+            verticalVelocity = jumpForce;
+        }
+
+        //verticalVelocity += gravity * Time.deltaTime;
+
+        Vector3 finalMovement = moveDir;
+        finalMovement.y = verticalVelocity;
+
+        transform.position += finalMovement * Time.deltaTime;
+
+
+        // Keep player from falling through the floor
+        if (transform.position.y < 0) transform.position = new Vector3(transform.position.x, 0, transform.position.z);
     }
 }
